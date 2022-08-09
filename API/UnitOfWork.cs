90c822f99b52c788e5f2bd787da5d6bf22ac78f0
Богдан Олейников <ol.bogdan13@gmail.com>
2022-08-10 00:22:57 +0300

    refactored UnitOfWork

--- API/UnitOfWork.cs
+++ API/UnitOfWork.cs
@@ -47,26 +47,17 @@ namespace API
         public IService<TEntity> GetService<TEntity>()
             where TEntity : BaseEntity
         {
-            if (_services == null)
-            {
-                _services = new Dictionary<Type, object>();
-            }
+            _services ??= new Dictionary<Type, object>();
 
             var type = typeof(TEntity);
             if (!_services.ContainsKey(type))
             {
-                switch (type.Name)
+                _ = type switch
                 {
-                    case "Material":
-                        _services[type] = new MaterialService(GetRepository<Material>());
-                        break;
-                    case "User":
-                        _services[type] = new UserService(GetRepository<User>());
-                        break;
-                    case "Course":
-                        _services[type] = new CourseService(GetRepository<Course>());
-                        break;
-                }
+                    { Name: "Material" } => _services[type] = new MaterialService(GetRepository<Material>()),
+                    { Name: "User" } => _services[type] = new UserService(GetRepository<User>()),
+                    { Name: "Course" } => _services[type] = new CourseService(GetRepository<Course>()),
+                };
             }
 
             return (IService<TEntity>)_services[type];
@@ -80,26 +71,17 @@ namespace API
         public IRepository<TEntity> GetRepository<TEntity>()
             where TEntity : BaseEntity
         {
-            if (_repositories == null)
-            {
-                _repositories = new Dictionary<Type, object>();
-            }
+            _repositories ??= new Dictionary<Type, object>();
 
             var type = typeof(TEntity);
             if (!_repositories.ContainsKey(type))
             {
-                switch (type.Name)
+                _ = type switch
                 {
-                    case "Material":
-                        _repositories[type] = new MaterialRepository(DbContext);
-                        break;
-                    case "User":
-                        _repositories[type] = new UserRepository(DbContext);
-                        break;
-                    case "Course":
-                        _repositories[type] = new CourseRepository(DbContext);
-                        break;
-                }
+                    { Name: "Material" } => _repositories[type] = new MaterialRepository(DbContext),
+                    { Name: "User" } => _repositories[type] = new UserRepository(DbContext),
+                    { Name: "Course" } => _repositories[type] = new CourseRepository(DbContext),
+                };
             }
 
             return (IRepository<TEntity>)_repositories[type];
