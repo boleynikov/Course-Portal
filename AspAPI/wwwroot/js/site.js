407036a9b5bd3c9d331535ddb58e4081a670b06b
Богдан Олейников <ol.bogdan13@gmail.com>
2022-09-24 17:58:49 +0300

    fixed view of course card on HomePage

--- AspAPI/wwwroot/js/site.js
+++ AspAPI/wwwroot/js/site.js
@@ -26,30 +26,3 @@
         }
     });
 };
-
-function openLoginModal(parameters) {
-    var headerTxt = parameters.txt;
-    var url = parameters.url;
-    var modal = $('#modal');
-
-    if (url === undefined) {
-        alert('Data miss')
-    }
-
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function (response) {
-            $('.modal-dialog');
-            modal.find(".modal-header").find('.modal-title').html(headerTxt);
-            modal.find(".modal-body").html(response);
-            modal.modal('show')
-        },
-        failure: function () {
-            modal.modal('hide')
-        },
-        error: function (response) {
-            alert(response.responseText);
-        }
-    });
-};
